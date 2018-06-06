4920c81c067c903ab55459a717e7ef782aa89dac
WANGYONGQIANG\wyq <wangyq_mail@163.com>
2018-06-06 16:30:11 +0800

    增加了reprofit的功能

---
+++ app/src/main/java/com/example/wyq/mytestproject/activity/ReProFitActivity.java
@@ -0,0 +1,19 @@
+package com.example.wyq.mytestproject.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.example.wyq.mytestproject.R;
+
+/**
+ * Created by wyq on 2018/6/6.
+ */
+
+public class ReProFitActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reprofit);
+    }
+}
